3bbc9e0af2f2773a81c0d3adfe1207f1f317da03
Pratik Umesh Thorat <pratikthorat195@gmail.com>
2024-01-23 12:53:20 +0530

    Reverse array code

---
+++ Easy_Java/Array_Codes/src/com/demo/array/Reverse_Array.java
@@ -0,0 +1,44 @@
+package com.demo.array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Reverse_Array
+{
+
+	public static void main(String[] args)
+	{
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("Enter the array size :");
+		int size=scanner.nextInt();
+		int array[]= new int[size];
+		System.out.println("Enter the element of an array:");
+		for(int i=0; i<size; i++)
+		{
+			System.out.println("Enter element"+(i + 1) + ": ");
+			array[i]=scanner.nextInt();
+		}
+		
+		System.out.println("Before Reverse : "+Arrays.toString(array));
+		
+		reverse(array);
+		
+		System.out.println("After Reverse : "+Arrays.toString(array));
+		
+	}
+	
+	static void reverse(int[] arr)
+	{
+		int i=0, j=arr.length-1;
+		while( i<j)
+		{
+			int temp= arr[i];
+			   arr[i]=arr[j];
+			   arr[j]=temp;
+			   i++;
+			   j--;
+		}
+		
+	}
+
+}
